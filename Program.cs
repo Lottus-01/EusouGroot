a511c34419b115f3853410092bc2f68ca537f262
Vittoria.Lopes <lopes.tt06@gmail.com>
2025-06-24 21:40:14 -0300

    apanhei

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿string Resposta;
+
+do
+{
+    Console.Write("Pergunta:");
+    Resposta = Console.ReadLine()!;
+    Console.WriteLine("Eu sou o Groot");
+}
+while(Resposta.ToUpper() != "TCHAU");
+
+    Console.WriteLine("Eu sou o Groot!");
